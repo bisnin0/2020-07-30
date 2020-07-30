6f2df9947320dc518f4c2dbaf4f8f50a2ed3292f
bisnin0 <dustmq299@gmail.com>
2020-07-30 16:18:23 +0900

    first commit

---
+++ src/LottoCollection.java
@@ -0,0 +1,22 @@
+
+
+public class LottoCollection {
+
+	public LottoCollection() {
+
+	}
+
+	
+	public void start() {
+		
+	}
+	public static void main(String[] args) {
+		new LottoCollection().start();
+
+	}
+
+}
+
+
+//배열사용금지
+//예외처리
